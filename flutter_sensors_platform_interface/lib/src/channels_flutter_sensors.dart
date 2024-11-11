d75ae9d41158239cce223af3426ec6ed371a0085
derrik.fleming <derrik.fleming@spindance.com>
2024-11-11 10:43:37 -0500

    feat: platform interface (#2)
    fix: platform interface

--- flutter_sensors_platform_interface/lib/src/channels_flutter_sensors.dart
+++ flutter_sensors_platform_interface/lib/src/channels_flutter_sensors.dart
@@ -3,13 +3,23 @@ import 'package:flutter/services.dart';
 import 'package:flutter_sensors_platform_interface/flutter_sensors_platform_interface.dart';
 
 /// An implementation of [FlutterSensorsPlatform] that uses method channels.
-class MethodChannelFlutterSensors extends FlutterSensorsPlatform {
+class ChannelsFlutterSensors extends FlutterSensorsPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('flutter_sensors');
 
+  /// The event channel used to recieve events from the native platform
+  @visibleForTesting
+  final EventChannel eventChannel =
+      const EventChannel('flutter_sensors/sensor_events');
+
   @override
   Future<String?> getPlatformName() {
     return methodChannel.invokeMethod<String>('getPlatformName');
+  }
+
+  @override
+  Stream<SensorData> get sensorData {
+    throw UnimplementedError();
   }
 }
