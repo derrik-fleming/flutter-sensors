f333571a465e252a98e0427d2657542072b67c00
derrik.fleming <derrik.fleming@spindance.com>
2024-11-11 12:38:57 -0500

    feat: android implementation (#2)

--- flutter_sensors_android/android/src/main/kotlin/com/spindance/tutorial/FlutterSensorsPlugin.kt
+++ flutter_sensors_android/android/src/main/kotlin/com/spindance/tutorial/FlutterSensorsPlugin.kt
@@ -1,30 +1,87 @@
 package com.spindance.tutorial
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
+import org.json.JSONObject
 
-class FlutterSensorsPlugin : FlutterPlugin, MethodCallHandler {
-    private lateinit var channel: MethodChannel
+class FlutterSensorsPlugin :
+    FlutterPlugin,
+    MethodChannel.MethodCallHandler,
+    EventChannel.StreamHandler,
+    SensorEventListener {
+    private lateinit var methodChannel: MethodChannel
+    private lateinit var eventChannel: EventChannel
+    private lateinit var sensorManager: SensorManager
+    private var eventSink: EventChannel.EventSink? = null
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_sensors_android")
-        channel.setMethodCallHandler(this)
+    override fun onAttachedToEngine(
+        @NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding,
+    ) {
+        methodChannel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_sensors_android")
+        methodChannel.setMethodCallHandler(this)
+        eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "flutter_sensors_android/sensor_events")
+        eventChannel.setStreamHandler(this)
+        sensorManager = flutterPluginBinding.applicationContext.getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
 
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    override fun onMethodCall(
+        @NonNull call: MethodCall,
+        @NonNull result: MethodChannel.Result,
+    ) {
         if (call.method == "getPlatformName") {
-            result.success("Android ${android.os.Build.VERSION.RELEASE}")            
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
         } else {
             result.notImplemented()
         }
     }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        channel.setMethodCallHandler(null)
+    override fun onDetachedFromEngine(
+        @NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding,
+    ) {
+        methodChannel.setMethodCallHandler(null)
     }
+
+    override fun onListen(
+        arguments: Any?,
+        events: EventChannel.EventSink?,
+    ) {
+        eventSink = events
+        startGyroscope()
+    }
+
+    override fun onCancel(arguments: Any?) {
+        stopGyroscope()
+        eventSink = null
+    }
+
+    private fun startGyroscope() {
+        sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)?.also { gyroscope ->
+            sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    private fun stopGyroscope() {
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_GYROSCOPE) {
+            val data = mapOf("type" to "gyro", "x" to event.values[0], "y" to event.values[1], "z" to event.values[2])
+            val jsonString = JSONObject(data).toString()
+            eventSink?.success(jsonString)
+        }
+    }
+
+    override fun onAccuracyChanged(
+        sensor: Sensor?,
+        accuracy: Int,
+    ) {}
 }
